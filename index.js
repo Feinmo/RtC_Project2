89ae82a957a0eec87e500b4dc970fe298e0ce438
Feinmo <136688223+Feinmo@users.noreply.github.com>
2024-02-26 16:02:51 +0100

    modify generateArticles function with target object

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 productsArray = [
-{name: "product 1", class: "staff", vendor: "vendor 1", price: 500, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?"},
-{name: "product 2", class: "electronics", vendor: "vendor 1", price: 1500, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?"},
-{name: "product 3", class: "random", vendor: "vendor 1", price: 800, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?"},
-{name: "product 4", class: "electronics", vendor: "vendor 1", price: 2500, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?"},
-{name: "product 5", class: "pets", vendor: "vendor 1", price: 25, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?"},
-{name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?"},
+    { name: "product 1", class: "staff", vendor: "vendor 1", price: 500, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 2", class: "electronics", vendor: "vendor 1", price: 1500, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 3", class: "random", vendor: "vendor 1", price: 800, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 4", class: "electronics", vendor: "vendor 1", price: 2500, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 5", class: "pets", vendor: "vendor 1", price: 25, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    { name: "product 6", class: "pets", vendor: "vendor 1", price: 350, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, magnam?" },
+    
 ];
 
 const articlesMain = document.querySelector(".articles-container");
@@ -20,12 +35,12 @@ populateArticle = (product) => {
     <h3>${product.price}</h4>`;
 }
 
-const generateArticles = ((Array) => {
+const generateArticles = ((Array, targetElement) => {
     Array.map((product) => {
-    const productElement = document.createElement("article");
-    productElement.innerHTML += populateArticle(product);
-    articlesMain.append(productElement);   
-});
+        const productElement = document.createElement("article");
+        productElement.innerHTML += populateArticle(product);
+        targetElement.append(productElement);
+    });
 });
 
-generateArticles(productsArray);
+generateArticles(productsArray, articlesMain);
